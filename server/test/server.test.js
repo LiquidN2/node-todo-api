3eaa2c80e05e5905413c2712b11650361f68b95e
Hugh Nguyen <hhnguyen255@gmail.com>
2018-07-30 12:57:50 +1000

    Add mongoose sample and GET /todos/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,9 +4,19 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
-/** Wipe Todo collection before each testing */
+const todosTestData = [
+    {text: "First todo text"},
+    {text: "Second todo text"}
+];
+
+/** Before each test:
+ * 1. wipe Todo collection
+ * 2. add dummy data
+ */
 beforeEach(done => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({})
+        .then(() => Todo.insertMany(todosTestData))
+        .then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -26,14 +36,14 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                /** check if the document is added to the todo collection
-                 * assuming todo collection has been wiped 
-                 */
-                Todo.find().then(todos => {
-                    expect(todos.length).toBe(1);
-                    expect(todos[0].text).toBe(text);
-                    done();
-                }).catch(err => done(err));
+                /** check if the document is added to the todo collection */
+                Todo.find({text})
+                    .then(todos => {
+                        expect(todos.length).toBe(1);
+                        expect(todos[0].text).toBe(text);
+                        done();
+                    })
+                    .catch(err => done(err));
             })
     });
 
@@ -47,10 +57,36 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then(todos => {
-                    expect(todos.length).toBe(0);
-                    done();
-                }).catch(err => done(err));
+                Todo.find()
+                    .then(todos => {
+                        expect(todos.length).toBe(todosTestData.length);
+                        done();
+                    })
+                    .catch(err => done(err));
+            });
+    });
+});
+
+
+describe('GET /todos', () => {
+    it('should get all todos docs', done => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todos.length).toBe(todosTestData.length);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.find()
+                    .then(todos => {
+                        expect(todos.length).toBe(todosTestData.length);
+                        done();
+                    })
+                    .catch(err => done(err));
             });
     });
 });
